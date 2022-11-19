48f8f4d56249a46e24a5efcdfb4780f82e05a6d1
user10 <work20go@mail.ru>
2022-11-19 13:13:56 +0200

    Добавьте файлы проекта.

---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,25 @@
+﻿namespace ClassLibrary
+{
+    public class Class1
+    {
+        public static double[] maxPair(double[,] mas)
+        {
+            int rows = mas.GetUpperBound(0)+1;
+            int cols = mas.Length / rows;
+            double[] res = new double[cols];
+            double max = mas[0, 0];
+            int index=-1;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if(mas[i, j] > max)
+                    {
+                        max = mas[i, j];
+                        index = j;
+                    }
+                }
+            }
+        }
+    }
+}
